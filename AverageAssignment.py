a9ee1eddd5bd2a6550baab8172bc0bcced02a936
Father-Hensley <jhensley0818@gmail.com>
2025-01-30 20:06:32 -0500

    Update AverageAssignment.py

---
+++ AverageAssignment.py
@@ -0,0 +1,20 @@
+#Print a line asking the user to enter three numbers
+#Prompt the user to enter the three integers, one at a time
+#Display the three numbers entered, their sum, and the average.
+#You can assume the user will only enter positive integers
+#Example output: (you do not have to match my words exactly)
+#Please enter three whole numbers:
+#Number 1: 11
+#Number 2: 11
+#Number 3: 12
+#The sum of 11 and 11 and 12 is 34 and the average is 11.333333333333334.
+print("Pick three numbers and we will find the sum and the average.")
+#Number 1
+A = 23
+#Number 2
+B = 12
+#Number 3
+C = 55
+D = A + B + C
+E = D /3
+print(f'The sum of your choices is {D}, and the average is {E}.')
